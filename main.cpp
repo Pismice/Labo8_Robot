848a1e0f334e1b92b19a49243b05d9973457228f
pismice <pismice15@gmail.com>
2022-01-14 13:34:05 +0100

    Coucou Didier :)

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : <nom du fichier>.<xxx> (xxx = h ou cpp)
+Auteur(s)       : Jérémie Santoro
+Date creation   : <jj.mm.aaaa>
+
+Description     : <à compléter>
+
+Remarque(s)     : <à compléter>
+
+Compilateur     : Mingw-w64 g++ 11.2.0
+-----------------------------------------------------------------------------------
+*/
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "Coucou Didier :)";
+
+ return EXIT_SUCCESS;
+}
