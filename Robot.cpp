f3ac3b643e9cf0dafb2ddbd4dafd49c56bf210ab
lokokpe <kossi.lokokpe@heig-vd.ch>
2022-01-20 23:06:35 +0100

    Auto stash before merge of "main" and "origin/main"

--- Robot.cpp
+++ Robot.cpp
@@ -27,16 +27,36 @@ void Robot::deplacer() {
 
    switch (d) {
       case int(Robot::Direction::HAUT) :
-
+          if(this->position.getY() <= 0){ // hauteur mini
+              setPosition(position.getX(), 2);
+          }else{
+              unsigned y = position.getY() + 1;
+              setPosition(position.getX(), y);
+          }
          break;
       case int(Robot::Direction::BAS) :
-
+          if(this->position.getY() >= 10){ // hauteur maxi
+              setPosition(position.getX(), 8);
+          }else{
+              unsigned y = position.getY() + 1;
+              setPosition(position.getX(), y);
+          }
          break;
       case int(Robot::Direction::DROITE) :
-
+          if(this->position.getX() >= 10){ // hauteur maxi
+              setPosition(8,position.getY());
+          }else{
+              unsigned x = position.getX() + 1;
+              setPosition(x, position.getY());
+          }
          break;
       case int(Robot::Direction::GAUCHE) :
-
+          if(this->position.getX() <= 0){ // hauteur maxi
+              setPosition(2,position.getY());
+          }else{
+              unsigned x = position.getX() + 1;
+              setPosition(x, position.getY());
+          }
          break;
    }
    //
--- Terrain.cpp
+++ Terrain.cpp
@@ -43,7 +43,6 @@ void Terrain::deploiement(unsigned nbrObjet) {
                   estUneCaseVierge = false;
                    break;
                }
-              estUneCaseVierge = true;
            }
            Robot r;
            r.setPosition(x,y);
